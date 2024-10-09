30bd339e375dc95f879fada028253b842b09d8ed
abhaydmathur <admathur@iitk.ac.in>
2024-10-09 15:58:31 +0200

    stashing

---
+++ src/data_utils/download_asv21.bash
@@ -0,0 +1,9 @@
+wget https://zenodo.org/records/4835108/files/ASVspoof2021_DF_eval_part00.tar.gz 
+wget https://zenodo.org/records/4835108/files/ASVspoof2021_DF_eval_part01.tar.gz
+wget https://zenodo.org/records/4835108/files/ASVspoof2021_DF_eval_part02.tar.gz
+wget https://zenodo.org/records/4835108/files/ASVspoof2021_DF_eval_part03.tar.gz
+
+tar -xvf ASVspoof2021_DF_eval_part00.tar.gz 
+tar -xvf ASVspoof2021_DF_eval_part01.tar.gz 
+tar -xvf ASVspoof2021_DF_eval_part02.tar.gz 
+tar -xvf ASVspoof2021_DF_eval_part03.tar.gz 
